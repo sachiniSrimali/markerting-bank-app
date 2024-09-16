a774aa4554a41b95b22859d2b2c371caed9abe34
sachini weragodavidana <sachinisrimali@gmail.com>
2024-09-16 10:27:34 +0200

    sticky navigation: Intersection observer API

--- script.js
+++ script.js
@@ -134,3 +134,38 @@ const handelHover = function (e) {
 // passing "argument" into handler
 nav.addEventListener('mouseover', handelHover.bind(0.5));
 nav.addEventListener('mouseout', handelHover.bind(1));
+
+// sticky navigation: Intersection observer API
+
+//const obsCallback = function (entries, observer) {
+//entries.forEach(entry => {
+// console.log(entry);
+//});
+//};
+
+//const obsOptions = {
+//root: null,
+//threshold: [0, 0.2],
+//};
+
+//const observer = new IntersectionObserver(obsCallback, obsOptions);
+//observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
